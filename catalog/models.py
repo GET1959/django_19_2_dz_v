adf3b87fde1a014a0b190498833c30dda474d67d
gennady <topchiev@mail.ru>
2024-02-21 17:53:33 +0000

    modified models.py

--- catalog/models.py
+++ catalog/models.py
@@ -2,14 +2,30 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('category',)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+
 
     def __str__(self):
         return self.name
@@ -19,15 +35,3 @@ class Product(models.Model):
         verbose_name_plural = 'Продукты'
         ordering = ('name',)
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ('name',)
