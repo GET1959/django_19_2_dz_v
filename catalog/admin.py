8285eb4f6dde617e3e9e58a651a2e7a27737ff1b
get <topchiev@mail.ru>
2024-03-03 20:37:39 +0300

    modified admin.py views.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,12 +4,12 @@ from catalog.models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "category")
+    list_display = ("id", "category",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "category",)
+    list_display = ("id", "name", "price", "category", "image",)
     list_filter = ("category",)
     search_fields = ("name", "description",)
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
+
 def index(request):
     return render(request, 'catalog/index.html')
 
+
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()[:3]
+    }
+    return render(request, 'catalog/product.html', context)
+
 
 def contacts(request):
     if request.method == 'POST':
@@ -13,3 +21,46 @@ def contacts(request):
         message = request.POST.get('message')
         print(f"name - {name}, phone - {phone}, message - {message}")
     return render(request, 'catalog/contacts.html')
+
+
+def product(request):
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/product.html', context)
+
+
+def product_item(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk)
+    }
+
+    return render(request, 'catalog/product_item.html', context)
+
+
+def category(request):
+    context = {
+        'object_list': Category.objects.all()
+    }
+    return render(request, 'base.html', context)
+
+
+def beverages(request):
+    context = {
+        'object_list': Product.objects.filter(category=1)
+    }
+    return render(request, 'catalog/product.html', context)
+
+
+def condiments(request):
+    context = {
+        'object_list': Product.objects.filter(category=2)
+    }
+    return render(request, 'catalog/product.html', context)
+
+
+def confections(request):
+    context = {
+        'object_list': Product.objects.filter(category=3)
+    }
+    return render(request, 'catalog/product.html', context)
